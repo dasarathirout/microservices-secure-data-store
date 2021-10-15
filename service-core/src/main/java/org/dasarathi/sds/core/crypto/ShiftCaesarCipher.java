1ce1f7dc5f0823e03f2df2e48123c7ae8ae77c5b
R.Dasarathi <dasarathi.rout@live.com>
2021-10-15 16:19:48 +0530

    Patch-08: Shift Caesar Cipher Class Implementation.

---
+++ service-core/src/main/java/org/dasarathi/sds/core/crypto/ShiftCaesarCipher.java
@@ -0,0 +1,48 @@
+package org.dasarathi.sds.core.crypto;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.logging.Logger;
+
+public final class ShiftCaesarCipher {
+    // https://en.wikipedia.org/wiki/Caesar_cipher
+    private static final Logger LOG = Logger.getLogger(ShiftCaesarCipher.class.getName());
+    private static String ASCII_FROM_32_TO_127 = "";
+    private static final int SHIFT = 95;
+
+    static {
+        for (int i = 32; i < 127; i++) {
+            ASCII_FROM_32_TO_127 += String.valueOf((char) i);
+        }
+    }
+
+    private ShiftCaesarCipher() {
+
+    }
+
+    public static final String encrypt(String inValue, int shiftKey) {
+        String cipherValue = "";
+        for (int i = 0; i < inValue.length(); i++) {
+            int charPosition = ASCII_FROM_32_TO_127.indexOf(inValue.charAt(i));
+            int keyVal = (shiftKey + charPosition) % SHIFT;
+            char replaceVal = ASCII_FROM_32_TO_127.charAt(keyVal);
+            cipherValue += replaceVal;
+        }
+        return cipherValue;
+    }
+
+    public static final String decrypt(String cipherValue, int shiftKey) {
+        String rawValue = "";
+        for (int i = 0; i < cipherValue.length(); i++) {
+            int charPosition = ASCII_FROM_32_TO_127.indexOf(cipherValue.charAt(i));
+            int keyVal = (charPosition - shiftKey) % SHIFT;
+            if (keyVal < 0) {
+                keyVal = ASCII_FROM_32_TO_127.length() + keyVal;
+            }
+            char replaceVal = ASCII_FROM_32_TO_127.charAt(keyVal);
+            rawValue += replaceVal;
+        }
+        return rawValue;
+    }
+
+}
