8039c6f4168fef001181358843389fb88fbec4f6
R.Dasarathi <dasarathi.rout@live.com>
2021-10-17 12:39:03 +0530

    Patch-25: Unique Mocked Sample Data Set

--- service-one/src/main/java/org/dasarathi/sds/one/data/MockedUser.java
+++ service-one/src/main/java/org/dasarathi/sds/one/data/MockedUser.java
@@ -1,14 +1,22 @@
-package org.dasarathi.sds.two.data;
+package org.dasarathi.sds.one.data;
 
 import org.dasarathi.sds.core.model.User;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class MockedUser {
     private static final int SIZE = 100;
     private static final Random CRAZY = new Random();
+    private static final HashSet<User> hashSetDummy = new LinkedHashSet<User>();
 
-    public static int update() {
+    static {
+        update();
+    }
+
+    private static int update() {
         for (int i = 0; i < SIZE; i++) {
             User mocked = new User();
             mocked.setId(CRAZY.nextInt(9999) + 1111);
@@ -18,10 +26,14 @@ public class MockedUser {
                     "-" + (CRAZY.nextInt(15) + 14));
             mocked.setSalary(CRAZY.nextDouble() + 9999);
             mocked.setCurrentAge(CRAZY.nextInt(50) + 20);
-            MemoryDB.addContents(mocked);
+            hashSetDummy.add(mocked);
         }
-        User canSeeMe = new User(111, "Dr.Rout ServiceTwo", "11-11-1999", 9999.0, 30);
-        MemoryDB.addContents(canSeeMe);
+        User canSeeMe = new User(111, "Dr.Rout ServiceOne", "11-11-1999", 9999.0, 30);
+        hashSetDummy.add(canSeeMe);
         return SIZE;
+    }
+
+    public static Set<User> searchAllUser() {
+        return hashSetDummy;
     }
 }
