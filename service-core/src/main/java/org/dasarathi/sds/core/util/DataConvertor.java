a491bda81086f514f13812f9d350e07d7ced75f7
R.Dasarathi <dasarathi.rout@live.com>
2021-10-18 08:33:23 +0530

    Patch-38: Data Converter.

--- service-core/src/main/java/org/dasarathi/sds/core/util/DataConvertor.java
+++ service-core/src/main/java/org/dasarathi/sds/core/util/DataConvertor.java
@@ -1,6 +1,7 @@
 package org.dasarathi.sds.core.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.dasarathi.sds.core.encrypt.UserEncryption;
 import org.dasarathi.sds.core.model.User;
 
@@ -11,11 +12,11 @@ public class DataConvertor {
     private static final ObjectMapper mapper = new ObjectMapper();
 
     public static User fromCSVValue() {
+        User model;
         return null;
     }
 
     public static User fromJSONValue(String inValue) {
-        ;
         User model;
         try {
             LOG.info("Starting JSON String To Obj ...");
@@ -28,8 +29,16 @@ public class DataConvertor {
         }
     }
 
-    public static User fromXMLValue() {
-        return null;
+    public static User fromXMLValue(String inValue) {
+        User model;
+        try {
+            LOG.info("Starting XML String To Obj ...");
+            model = mapper.readValue(inValue, User.class);
+            return model;
+        } catch (Exception ex) {
+            LOG.severe("JSON Content Parsing Error : " + ex.getMessage());
+            throw new RuntimeException();
+        }
     }
 
 }
